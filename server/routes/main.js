52249d4a2199e9a38618cb7d39ef3851983c9a83
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-04-19 22:29:36 +0300

    register

--- server/routes/main.js
+++ server/routes/main.js
@@ -33,6 +33,10 @@ router.get('/login',(req, res) => {
     res.render('login')
 })
 
+router.get('/register',(req, res) => {
+    res.render('register')
+})
+
 router.get('/cart',(req, res) => {
     res.render('cart')
 })
